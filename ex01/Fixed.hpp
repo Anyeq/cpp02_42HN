9c45b5a4c6c62480f7f40d7c16de0044c0c633b1
asando <alexando17@yahoo.com>
2026-06-26 21:34:03 +0200

    ADD: Fixed.hpp classes

--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,34 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/26 21:08:57 by asando            #+#    #+#             */
-/*   Updated: 2026/06/26 21:08:59 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/26 21:33:12 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int					_rawBits;
+		static const int	_fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& rhs);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+
+#endif
