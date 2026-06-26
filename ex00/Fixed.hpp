1bcd9414d24561c85461941cecec22b4f72d9108
asando <alexando17@yahoo.com>
2026-06-26 12:13:34 +0200

    Fixed restructure ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,12 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/20 13:41:15 by asando            #+#    #+#             */
-/*   Updated: 2026/06/20 13:41:16 by asando           ###   ########.fr       */
+/*   Created: 2026/06/20 13:41:38 by asando            #+#    #+#             */
+/*   Updated: 2026/06/26 12:12:31 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	Fixed {
+	private:
+		int					_rawBits;
+		static const int	_fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& rhs);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/20 13:41:38 by asando            #+#    #+#             */
-/*   Updated: 2026/06/20 13:41:39 by asando           ###   ########.fr       */
+/*   Created: 2026/06/20 13:41:15 by asando            #+#    #+#             */
+/*   Updated: 2026/06/26 12:12:30 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void) {
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
