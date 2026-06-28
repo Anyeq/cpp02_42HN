1e29bfb2e81ca7003b12a94ba8fde31fb5bc315b
asando <alexando17@yahoo.com>
2026-06-28 12:42:23 +0200

    WIP: ex02 adding function

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,71 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 11:07:17 by asando            #+#    #+#             */
-/*   Updated: 2026/06/28 11:07:18 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/28 12:41:59 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <cmath>
+
+Fixed::Fixed() : _rawBits(0) {}
+
+Fixed::Fixed(const int value) {
+	_rawBits = value << _fractionalBits;
+}
+
+Fixed::Fixed(const float value) {
+	_rawBits = roundf(value * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed& other) {
+	*this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs) {
+	if (this != &rhs)
+		_rawBits = rhs._rawBits;
+	return *this;
+}
+
+Fixed::~Fixed() {}
+
+int	Fixed::getRawBits(void) const {
+	return _rawBits;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	_rawBits = raw;
+}
+
+float	Fixed::toFloat(void) const {
+	return (float)_rawBits / (float)(1 << _fractionalBits);
+}
+
+int	Fixed::toInt(void) const {
+	return _rawBits >> _fractionalBits;
+}
+
+bool	Fixed::operator>(const Fixed& rhs) const {
+	return _rawBits > rhs._rawBits;
+}
+
+bool	Fixed::operator<(const Fixed& rhs) const {
+	return _rawBits < rhs._rawBits;
+}
+
+bool	Fixed::operator>=(const Fixed& rhs) const {
+	return _rawBits >= rhs._rawBits;
+}
+
+bool	Fixed::operator<=(const Fixed& rhs) const {
+	return _rawBits <= rhs._rawBits;
+}
+
+bool	Fixed::operator==(const Fixed& rhs) const {
+	return _rawBits == rhs._rawBits;
+}
+
+bool	Fixed::operator!=(const Fixed& rhs) const {
+	return _rawBits != rhs._rawBits;
+}
--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,54 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 11:07:07 by asando            #+#    #+#             */
-/*   Updated: 2026/06/28 11:07:08 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/28 12:23:54 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+
+class	Fixed {
+	private:
+		int					_rawBits;
+		static const int	_fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed& other);
+		Fixed&	operator=(const Fixed& rhs);
+		~Fixed();
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		bool	operator>(const Fixed& rhs) const;
+		bool	operator<(const Fixed& rhs) const;
+		bool	operator>=(const Fixed& rhs) const;
+		bool	operator<=(const Fixed& rhs) const;
+		bool	operator==(const Fixed& rhs) const;
+		bool	operator!=(const Fixed& rhs) const;
+
+		Fixed	operator+(const Fixed& rhs) const;
+		Fixed	operator-(const Fixed& rhs) const;
+		Fixed	operator*(const Fixed& rhs) const;
+		Fixed	operator/(const Fixed& rhs) const;
+
+		Fixed&	operator++();
+		Fixed&	operator++(int);
+		Fixed&	operator--();
+		Fixed&	operator--(int);
+
+		static Fixed&		min(Fixed& a, Fixed& b);
+		static const Fixed&	min(const Fixed& a, const Fixed& b);
+		static Fixed&		max(Fixed& a, Fixed& b);
+		static const Fixed&	max(const Fixed& a, const Fixed& b);
+};
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& fixed);
+#endif
