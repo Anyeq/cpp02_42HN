f35fb461aa59c198dffcb2fb81c60ae6d0b992ab
asando <alexando17@yahoo.com>
2026-06-28 17:28:13 +0200

    ADD: test in main fucntion

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,23 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/28 11:06:56 by asando            #+#    #+#             */
-/*   Updated: 2026/06/28 11:06:58 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/28 17:27:52 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int	main(void) {
+	Fixed a;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return 0;
+}
