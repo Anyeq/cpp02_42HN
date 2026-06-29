2c426acb8c1d982092e1ddf31396ab503b89d59d
asando <alexando17@yahoo.com>
2026-06-29 14:25:24 +0200

    WIP: Implement Point header

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,28 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 14:17:55 by asando            #+#    #+#             */
-/*   Updated: 2026/06/29 14:17:57 by asando           ###   ########.fr       */
+/*   Updated: 2026/06/29 14:25:08 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef POINT_HPP
+# define POINT_HPP
+
+# include "Fixed.hpp"
+class	Point {
+	private:
+		const Fixed _x;
+		const Fixed _y;
+
+	public:
+		Point();
+		Point(const float x, const float y);
+		Point(const Point& other);
+		Point& operator=(const Point& rhs);
+		~Point();
+
+		Fixed	getX(void) const;
+		Fixed	getY(void) const;
+};
+
+#endif
